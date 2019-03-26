a67bab0b2bbf7363530ab71ffee294d0055618c1
leinefran <penny.leine@gmail.com>
2019-03-26 03:19:54 +0000

    Changed: body of function

--- 0x01-warm_up/2-binary_tree_is_full.c
+++ 0x01-warm_up/2-binary_tree_is_full.c
@@ -9,15 +9,19 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (!tree)
+	int left = 0, right = 0;
+
+	if (tree == NULL)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (1);
+	if (tree->left)
+		left = 1 + binary_tree_is_full(tree->left);
 
-	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left) &&
-			binary_tree_is_full(tree->right));
+	if (tree->right)
+		right = 1 + binary_tree_is_full(tree->right);
+
+	if (left - right == 0)
+		return (1);
 
 	return (0);
 }
