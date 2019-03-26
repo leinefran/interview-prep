ba9a21a3431cf5ef963a86182ad84db0d328eac1
leinefran <penny.leine@gmail.com>
2019-03-26 02:55:41 +0000

    Added: data structure

--- 0x01-warm_up/search_algos.h
+++ 0x01-warm_up/search_algos.h
@@ -4,6 +4,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
 int binary_search(int *array, size_t size, int value);
 int binary_tree_is_full(const binary_tree_t *tree);
 
