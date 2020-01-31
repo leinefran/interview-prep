803e32c8100e9d4c9cf309b0ce5d46a68e5aefe7
Leine Francisca Valente <cozymachete@Leines-MacBook-Pro.local>
2020-01-31 08:18:52 -0800

    Fixed: style.

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
-'''Problem: You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes. Write a method that determines if all the boxes can be opened.'''
+'''Problem: You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 and
+each box may contain keys to the other boxes.
+Write a method that determines if all the boxes can be opened.'''
+
 
 def canUnlockAll(boxes):
     if len(boxes) == 0:
         return False
 
-    opened_boxes = [0,]
+    opened_boxes = [0, ]
     number_of_boxes = len(boxes) - 1
 
     '''iterate through the outer list, and inner lists.'''
@@ -21,4 +25,3 @@ def canUnlockAll(boxes):
                     if len(opened_boxes) == len(boxes):
                         return True
     return False
-            
