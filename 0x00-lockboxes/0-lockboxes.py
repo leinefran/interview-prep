bd179697c1b49824e5bf2ff07322f3bc6d847fec
Leine Francisca Valente <cozymachete@Leines-MacBook-Pro.local>
2020-02-05 09:56:34 -0800

    Updated: shebang.

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''Problem: You have n number of locked boxes in front of you.
 Each box is numbered sequentially from 0 to n - 1 and
 each box may contain keys to the other boxes.
